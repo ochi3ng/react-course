89a974a9c9e022922c1241401d5badd8e101b4b2
ochi3ng <johnochi3ng@gmail.com>
2023-01-13 14:28:42 +0300

    working on styling

--- src/ExpenseItem.js
+++ src/ExpenseItem.js
@@ -1,24 +1,25 @@
-import React, { useState } from 'react'
-import './ExpenseItem.css'
-import ExpenseDate from './ExpenseDate'
+import { useState } from 'react';
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
 
-const ExpenseItem = (props)=> {
-const [title, setTitle]= useState(props.title);
+function ExpenseItem(props) {
+    const [title, setTitle]= useState(props.title);
 
-const clickHandler= ()=>{
-    setTitle('updated!');
-    console.log(title);
-};
+    const onclickHandler=()=>{
+        setTitle('updated!')
+        console.log('updated!')
+    }
   return (
-    <div className='expenses-item'>
-        <ExpenseDate date={props.date}/>
-        <div className='expenses-item_description'>
-         <h2>{title}</h2>
-         <div className='expenses-item_price'>${props.amount}</div>
-        </div>
-        <button onClick={clickHandler}>Change Title</button>
+    <div className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+
+      </div>
+      <button onClick={onclickHandler}>Change Title</button>
     </div>
-  )
+  );
 }
 
-export default ExpenseItem
+export default ExpenseItem;
